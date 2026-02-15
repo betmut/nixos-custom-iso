f9212a4244104c0aa9d6732eff46a5978f03a429
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-15 19:12:44 +0700

    add: add htop

--- home.nix
+++ home.nix
@@ -2,7 +2,7 @@
   home.stateVersion = "25.11";
 
   #Packages
-  home.packages = with pkgs; [nodejs python3 R gemini-cli];
+  home.packages = with pkgs; [nodejs python3 R gemini-cli htop];
   
   #VIM config
   programs.vim = {
