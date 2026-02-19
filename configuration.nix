43b353fe359bb144edef30b758d2e8e5d917d9ef
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-19 14:51:48 +0700

    fix: fix minor error

--- configuration.nix
+++ configuration.nix
@@ -3,7 +3,7 @@
   system.stateVersion = "25.11";
   imports = [
     ./modules/common.nix
-    ./modules/darwin.nix
+    #./modules/darwin.nix
     ./modules/linux.nix
   ];
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
