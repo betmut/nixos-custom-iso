05390283f319bd54012bc73c5227d1f8c80cd5d8
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-19 16:24:04 +0700

    fix: fix minor error

--- modules/linux.nix
+++ modules/linux.nix
@@ -1,17 +1,4 @@
-{config, pkgs, lib, inputs,  ... }: lib.mkIf pkgs.stdenv.isLinux {
-    #Zsh Shell
-    programs.zsh.enable = true;
-    users.users.nixos = {
-        shell = pkgs.zsh;
-        isNormalUser = true;
-        extraGroups = ["wheel"];
-        initialPassword = "";
-    };
-    
-    #Enable facetimehd
-    hardware.facetimehd.enable = true;
-    hardware.enableRedistributableFirmware = true;
-
+{config, pkgs, lib, inputs,  ... }: {
     # Allow proprietary software (Required for Broadcom)
     nixpkgs.config = {
         allowUnfree = true;
@@ -21,30 +8,45 @@
             "broadcom-sta-6.30.223.271-59-6.18.12"
         ];
     };
-
-    # Support for Broadcom BCM4360
-    boot = {
-        initrd.kernelModules = [ "wl" ];
-        kernelModules = [ "wl" ];
-        extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
-    };
     
-    # Networking
-    networking.networkmanager.enable = true;
-    
-    services = {
-        #thermald
-        thermald.enable = true;
+    config = lib.mkIf pkgs.stdenv.isLinux {
+        #Zsh Shell
+        programs.zsh.enable = true;
+        users.users.nixos = {
+            shell = pkgs.zsh;
+            isNormalUser = true;
+            extraGroups = ["wheel"];
+            initialPassword = "";
+        };
         
-        #mbpfan -- fan controller daemon for Apple Macs and MacBook
-        mbpfan = {
-            enable = true;
-            aggressive = false;
-            settings = {
-                general = {
-                    low_temp = 63;  # If temperature is below this, fans will run at minimum speed.
-                    high_temp = 66; # If temperature is above this, fan speed will gradually increase.
-                    max_temp = 86; # If temperature is above this, fans will run at maximum speed.
+        #Enable facetimehd
+        hardware.facetimehd.enable = true;
+        hardware.enableRedistributableFirmware = true;
+
+        # Support for Broadcom BCM4360
+        boot = {
+            initrd.kernelModules = [ "wl" ];
+            kernelModules = [ "wl" ];
+            extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+        };
+        
+        # Networking
+        networking.networkmanager.enable = true;
+        
+        services = {
+            #thermald
+            thermald.enable = true;
+            
+            #mbpfan -- fan controller daemon for Apple Macs and MacBook
+            mbpfan = {
+                enable = true;
+                aggressive = false;
+                settings = {
+                    general = {
+                        low_temp = 63;  # If temperature is below this, fans will run at minimum speed.
+                        high_temp = 66; # If temperature is above this, fan speed will gradually increase.
+                        max_temp = 86; # If temperature is above this, fans will run at maximum speed.
+                    };
                 };
             };
         };
