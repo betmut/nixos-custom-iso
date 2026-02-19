ee69e06fcbf1b1653edf973ba2728dd081e77fcd
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-19 16:47:06 +0700

    fix: fix minor error

--- flake.nix
+++ flake.nix
@@ -45,6 +45,7 @@
       inputs.home-manager.nixosModules.home-manager
       {
         home-manager = {
+          extraSpecialArgs = { inherit inputs; };
           useGlobalPkgs = true;
           useUserPackages = true;
           users.${user} = ./home.nix;
