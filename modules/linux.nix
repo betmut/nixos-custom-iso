479940d494ff17fac00fa8e66aa9e1191f2bce64
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-20 08:23:57 +0700

    fix : refactor little bit

--- modules/linux.nix
+++ modules/linux.nix
@@ -1,17 +1,21 @@
-{config, pkgs, inputs,  ... }: {
-    #Zsh Shell
-    programs.zsh.enable = true;
-    users.users.nixos = {
+{config, pkgs, inputs,  ... }: 
+let
+    userDefaults = {
         shell = pkgs.zsh;
         isNormalUser = true;
         extraGroups = ["wheel"];
         initialPassword = "";
     };
-    
+in
+{
+    #Zsh Shell
+    programs.zsh.enable = true;
+    users.users.nixos = userDefaults;
+   
     #Enable facetimehd
     hardware.facetimehd.enable = true;
     hardware.enableRedistributableFirmware = true;
-
+    
     # Allow proprietary software (Required for Broadcom)
     nixpkgs.config = {
         allowUnfree = true;
@@ -28,14 +32,11 @@
         kernelModules = [ "wl" ];
         extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
     };
-    
     # Networking
     networking.networkmanager.enable = true;
-    
     services = {
         #thermald
         thermald.enable = true;
-        
         #mbpfan -- fan controller daemon for Apple Macs and MacBook
         mbpfan = {
             enable = true;
