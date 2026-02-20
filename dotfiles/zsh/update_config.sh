83cd8e6863380d20dbc823e89322750eefa975ce
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-21 03:46:22 +0700

    fix: fix the update_config.sh

--- dotfiles/zsh/update_config.sh
+++ dotfiles/zsh/update_config.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 update_config() {
   local target_os=${1:-$(uname | tr '[:upper:]' '[:lower:]')}
-  local file="configuration.nix"
+  local file="../../configuration.nix"
 
   # Memastikan file ada sebelum diproses
   if [ ! -f "$file" ]; then
@@ -11,13 +13,15 @@ update_config() {
   if [ "$target_os" == "linux" ]; then
     echo "Changing configuration to Linux..."
     # Searching 'darwin' and replace it with 'linux'
-    sed -i 's/darwin/linux/g' "$file"
+    sed -i 's/darwin/linux/g' "$file" && git add $file
   else
     echo "Changing configuration to Darwin (macOS)..."
     # Searching 'linux' and replace it with 'darwin'
-    sed -i 's/linux/darwin/g' "$file"
+    sed -i 's/linux/darwin/g' "$file" && git add $file
   fi
 
   echo "Selesai! Berikut isi file sekarang:"
   grep -E "linux|darwin" "$file"
 }
+
+update_config
