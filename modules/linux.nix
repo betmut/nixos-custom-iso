5d082661e7ef43924c5709fc14061a1be1cd6418
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-02-24 21:14:36 +0000

    add: modifying little bit

--- modules/linux.nix
+++ modules/linux.nix
@@ -30,7 +30,7 @@
 
     # Support for Broadcom BCM4360
     boot = {
-	initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod"]
+	initrd.availableKernelModules = ["xhci_pci" "nvme" "usb_storage" "usbhid" "sd_mod"];
         initrd.kernelModules = [ "wl" ];
         kernelModules = [ "wl" "kvm-intel" ];
         extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
