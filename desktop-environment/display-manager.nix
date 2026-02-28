0e91b5b2175afce2d2e542106f871c850f01dca8
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-03-01 03:55:31 +0700

    add: add tuigreet theme

--- desktop-environment/display-manager.nix
+++ desktop-environment/display-manager.nix
@@ -5,7 +5,7 @@
     settings = {
       default_session = {
         command = let 
-          theme = "border=magenta;text=cyan;prompt=green;container=black;greet=bright_cyan;time=bright_green";
+          theme = "border=magenta;text=green;prompt=green;container=black;greet=blue;time=green";
           greeting = "Welcome back. Ready to dive in?";
         in
         "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland --theme ${theme} --greeting ${greeting}";
