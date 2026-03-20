35a269d192c4901b9dbe72cd083d222f2cfde1f8
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-03-20 22:46:57 +0700

    fix: fix path to hyprpaper

--- users/mathewelhans/config/hyprpaper.nix
+++ users/mathewelhans/config/hyprpaper.nix
@@ -2,7 +2,7 @@
   let 
     wallhavenWallpaper = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/AngelJumbo/gruvbox-wallpapers/refs/heads/main/wallpapers/anime/gruv-wallhaven-m9e9m1.png";
-      sha256 = "ssha256-v9P4EO3gM39F5YI2o4wK+ZKYAtDC4/9Zo16GrQWct78=";
+      sha256 = "sha256-v9P4EO3gM39F5YI2o4wK+ZKYAtDC4/9Zo16GrQWct78=";
     };
   in {
   #Hyprpaper config
