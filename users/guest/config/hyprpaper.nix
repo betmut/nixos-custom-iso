d53e9cf105b5de1be3eb8d58a1e191bb82734812
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-03-20 22:44:26 +0700

    fix: fix path to hyprpaper

--- users/guest/config/hyprpaper.nix
+++ users/guest/config/hyprpaper.nix
@@ -2,7 +2,7 @@
   let 
     kurumiWallpaper = pkgs.fetchurl {
       url = "https://raw.githubusercontent.com/AngelJumbo/gruvbox-wallpapers/refs/heads/main/wallpapers/anime/Kurumi-Ebisuzawa.png";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+      sha256 = "sha256-M9eZZeJWmAT1R6k+wLj2f4Rj0c1Yz90CB4du0CFIwuY=";
     };
   in {
   #Hyprpaper config
