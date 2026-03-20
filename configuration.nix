410fde9e140ceffbff23e0baa816116be9925e30
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-03-20 13:48:44 +0700

    add: add my user for trusted-users

--- configuration.nix
+++ configuration.nix
@@ -13,7 +13,6 @@
       substituters = ["https://hyprland.cachix.org"];
       trusted-substituters = ["https://hyprland.cachix.org"];
       trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-      allowed-users  = ["root" "@wheel"]; 
     };
     gc = {
       automatic = true;
