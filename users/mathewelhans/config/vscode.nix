067843baf89c0fb7e09604e736cfed66df3e2088
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-04-12 08:50:49 +0700

    add racket

--- users/mathewelhans/config/vscode.nix
+++ users/mathewelhans/config/vscode.nix
@@ -22,7 +22,7 @@
           name = "vscode-scheme";
           publisher = "sjhuangx"; 
           version = "0.4.0";
-          sha256 = "";
+          sha256 = "BN+C64YQ2hUw5QMiKvC7PHz3II5lEVVy0Shtt6t3ch8=";
         }
       ];
        
