106d03279e7268d1873c144f0bc074aff5a8287b
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-04-16 06:10:30 +0700

    change wallpaper

--- users/mathewelhans/config/hyprpaper.nix
+++ users/mathewelhans/config/hyprpaper.nix
@@ -1,8 +1,7 @@
 {config, pkgs, lib,...}: 
   let 
-    githubUrl = "https://raw.githubusercontent.com/AngelJumbo/gruvbox-wallpapers/refs/heads/main";
-    wallhavenWallpaper = pkgs.fetchurl {
-      url = "${githubUrl}/wallpapers/anime/gruv-wallhaven-m9e9m1.png";
+    wallpaper = pkgs.fetchurl {
+      url = "https://gruvbox-wallpapers.pages.dev/wallpapers/anime/totoro_bus_stand.png";
       sha256 = "sha256-v9P4EO3gM39F5YI2o4wK+ZKYAtDC4/9Zo16GrQWct78=";
     };
   in {
@@ -14,7 +13,7 @@
       wallpaper = [
         {
           monitor = "eDP-1";
-          path = "${wallhavenWallpaper}";
+          path = "${wallpaper}";
         }
       ];
     };
