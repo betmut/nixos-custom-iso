a6382dc4d98566184ef17866b62b0da7b70f3abc
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-05-13 18:02:24 +0700

    fixing nix_shell_prompt.sh

--- dotfiles/shell/nix_shell_prompt.sh
+++ dotfiles/shell/nix_shell_prompt.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-NIX_SHELL_CUSTOM_PROMPT="export PS1='\[\033[1;34m\](nix-dev) \[\033[1;32m\][\u@\h] $ \[\033[00m\]'; return"
+
+NIX_SHELL_CUSTOM_PROMPT="export PS1='\[\033[1;34m\]❄️ (nix-dev) \[\033[1;32m\][\u@\h] $ \[\033[00m\]'; return"
 
 nix-shell-packages() {
   nix-shell -p "$@" --command $NIX_SHELL_CUSTOM_PROMPT
