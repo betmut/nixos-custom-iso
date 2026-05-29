2f9187c6201f6ef6889f62be102c11683c621f5d
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-05-29 11:04:48 +0700

    add lua config and some modules

---
+++ dotfiles/hyprland/hyprland.lua
@@ -0,0 +1,8 @@
+require("modules/monitor")
+require("modules/autostart")
+
+
+-- Set programs that you use
+local terminal    = "kitty"
+local fileManager = "thunar"
+local menu        = "rofi -show drun"
