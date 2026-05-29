f5471db68c238b1449ac14db16af91e21aa81da2
betmut <3011mathewelhans@apps.ipb.ac.id>
2026-05-30 02:13:12 +0700

    add keybindings, windows and workspace modules and minor tweak waybar configurations for tidying its syntax

--- dotfiles/hyprland/hyprland.lua
+++ dotfiles/hyprland/hyprland.lua
@@ -1,8 +1,63 @@
 require("modules/monitor")
 require("modules/autostart")
-
+require("modules/env_variable")
+require("modules/permissions")
+require("modules/animations")
+require("modules/keybindings")
+require("modules/windows-and-workspaces")
 
 -- Set programs that you use
 local terminal    = "kitty"
 local fileManager = "thunar"
 local menu        = "rofi -show drun"
+local screenshot  = 'grim -g "$(slurp)" - | swappy -f -'
+
+-- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
+hl.config({
+    general = {
+        gaps_in  = 5,
+        gaps_out = 15,
+
+        border_size = 2,
+
+        col = {
+            active_border   = { colors = {"rgba(8ec07ce6)", "rgba(89d6ae6)"}, angle = 45 },
+            inactive_border = "rgba(595959aa)",
+        },
+
+        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false,
+
+        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+        allow_tearing = false,
+
+        layout = "dwindle",
+    },
+
+    decoration = {
+        rounding       = 10,
+        rounding_power = 2,
+
+        -- Change transparency of focused and unfocused windows
+        active_opacity   = 1.0,
+        inactive_opacity = 1.0,
+
+        shadow = {
+            enabled      = true,
+            range        = 4,
+            render_power = 3,
+            color        = 0xee1a1a1a,
+        },
+
+        blur = {
+            enabled   = true,
+            size      = 8,
+            passes    = 2,
+            vibrancy  = 0.1696,
+        },
+    },
+
+    animations = {
+        enabled = true,
+    },
+})
--- dotfiles/hyprland/modules/autostart.lua
+++ dotfiles/hyprland/modules/autostart.lua
@@ -10,7 +10,7 @@
 
 
 hl.on("hyprland.start", function () 
-  hl.exec_cmd(terminal)
   hl.exec_cmd("hyprpaper")
   hl.exec_cmd("waybar & hypridle & swayosd-server & blueman-applet")
+  hl.exec_cmd("nm-applet & swaync")
 end)
